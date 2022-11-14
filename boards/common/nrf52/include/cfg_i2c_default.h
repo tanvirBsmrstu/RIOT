bdbb6c775d6db7411096c15bbccd9f5581da4ed1
Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
2022-11-14 13:16:09 +0100

    pkg/cryptoauthlib: Add PSA wrappers

--- boards/common/nrf52/include/cfg_i2c_default.h
+++ boards/common/nrf52/include/cfg_i2c_default.h
@@ -21,6 +21,7 @@
 #define CFG_I2C_DEFAULT_H
 
 #include "periph_cpu.h"
+#include "kernel_defines.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -36,7 +37,15 @@ static const i2c_conf_t i2c_config[] = {
         .scl = 27,
         .sda = 26,
         .speed = I2C_SPEED_NORMAL
+    },
+#ifdef BOARD_NRF52840DK
+    {
+        .dev = NRF_TWIM0,
+        .scl = 28,
+        .sda = 29,
+        .speed = I2C_SPEED_NORMAL
     }
+#endif
 };
 #define I2C_NUMOF           ARRAY_SIZE(i2c_config)
 /** @} */
