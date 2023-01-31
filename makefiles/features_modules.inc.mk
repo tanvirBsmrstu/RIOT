92d06ca547fd72f3999e96ceeffbb4fe1aec5afb
Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
2023-01-31 17:51:36 +0100

    fixup! Define general symbols and modules for HW capabilities

--- makefiles/features_modules.inc.mk
+++ makefiles/features_modules.inc.mk
@@ -11,12 +11,12 @@ USEMODULE += $(PERIPH_FEATURES)
 
 # Add all USED periph_% init modules unless they are blacklisted
 PERIPH_IGNORE_MODULES := \
-	periph_cipher_aes_128_cbc \
+  periph_cipher_aes_128_cbc \
   periph_clic \
   periph_common \
   periph_coretimer \
   periph_ecc_p192r1 \
-	periph_ecc_p256r1 \
+  periph_ecc_p256r1 \
   periph_eth \
   periph_eth_common \
   periph_flash \
