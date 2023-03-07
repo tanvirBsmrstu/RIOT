b5dc4b961f20e94ad38287795206cc8e8978a92f
Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
2023-03-07 16:51:02 +0100

    makefiles: remove unnecessary pseudomodule

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -58,7 +58,6 @@ PSEUDOMODULES += dhcpv6_client_mud_url
 PSEUDOMODULES += dhcpv6_relay
 PSEUDOMODULES += dns_cache
 PSEUDOMODULES += dns_msg
-PSEUDOMODULES += driver_cryptocell_310
 PSEUDOMODULES += ecc_%
 PSEUDOMODULES += ethos_stdio
 PSEUDOMODULES += event_%
