6b55dd1dcef43076c824c9227ad9d32f3f424e3a
tanvirBsmrstu <tanvir.bsmrstu@gmail.com.com>
2024-01-03 01:22:24 +0100

    gnrc_wolfssl_tls c file cleanup

--- examples/azure_pnp_demo/tools/network_config.sh
+++ examples/azure_pnp_demo/tools/network_config.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "configuring network interface"
 #sudo ../../dist/tools/tapsetup/tapsetup -d
-sudo ../../dist/tools/tapsetup/tapsetup -c 1
+sudo ../../../dist/tools/tapsetup/tapsetup -c 1
 sudo ip address add fd12:dead:beef::1/64 dev tapbr0
 #sudo ip a a fec0:affe::1/64 dev tapbr0
 #sudo ip a a 192.168.1.10/24 dev tapbr0
